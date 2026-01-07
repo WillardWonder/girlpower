342effedc7712af6adcdb3913e2700a15d9b526c
WillardWonder <126423311+WillardWonder@users.noreply.github.com>
2026-01-07 12:46:46 -0600

    Add localDateKey function to format dates

---
+++ src/lib/date.js
@@ -0,0 +1,6 @@
+export function localDateKey(d = new Date()) {
+  const yr = d.getFullYear();
+  const mo = String(d.getMonth() + 1).padStart(2, "0");
+  const da = String(d.getDate()).padStart(2, "0");
+  return `${yr}-${mo}-${da}`;
+}
